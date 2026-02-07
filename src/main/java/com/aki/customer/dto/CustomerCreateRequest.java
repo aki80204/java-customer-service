f6fd82f58b59c9e3faa0db3a5841d820c07bbce1
ユーザー <@AB350M-HDV.mshome.net>
2026-02-08 00:46:16 +0900

    update validation code

--- src/main/java/com/aki/customer/dto/CustomerCreateRequest.java
+++ src/main/java/com/aki/customer/dto/CustomerCreateRequest.java
@@ -4,9 +4,13 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerCreateRequest {
   @NotBlank(message = "名前は必須です")
   @Size(max = 255)
